ff0bd4aac0d41dbc8cb77428f4a4156dff05cae6
Fabio Avila <fabioravila@gmail.com>
2015-12-10 21:12:41 -0200

    # Add playground project to easy prototype

---
+++ Playground/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RiveScript;
+using RiveScript.lang;
+
+namespace Playground
+{
+    public static class T
+    {
+        public static string M()
+        {
+            return "oi";
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //It is an experience space for the library being developed
+
+            var rs = new RiveScript.RiveScript();
+            rs.setHandler("csharp", new CSharp());
+            rs.stream(new[] { "",
+                              "",
+                              "",
+                              "+ show context data",
+                              "- data is: <call>context</call>",
+                              "",
+                              "> object context csharp",
+                              "    using Playground;",
+                              "    return T.M();",
+                              "< object",
+                              ""
+            });
+
+
+            rs.sortReplies();
+
+
+
+            var result = rs.reply("default", "show context data");
+
+        }
+    }
+}
---
+++ RiveScript.Tests/Helper/ContextHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RiveScript.Tests.Helper
+{
+    public static class ContextHelper
+    {
+        public static string Property = "Context_Property";
+
+        /// <summary>
+        /// Allows setting the Entry Assembly when needed. 
+        /// Use AssemblyUtilities.SetEntryAssembly() as first line in ad hoc tests
+        /// </summary>
+        /// <param name="assembly">Assembly to set as entry assembly</param>
+        public static void SetEntryAssembly(Assembly assembly)
+        {
+            var manager = new AppDomainManager();
+            if (manager != null)
+            {
+                var entryAssemblyfield = manager.GetType().GetField("m_entryAssembly", BindingFlags.Instance | BindingFlags.NonPublic);
+                entryAssemblyfield.SetValue(manager, assembly);
+            }
+
+            var domain = AppDomain.CurrentDomain;
+            if (domain != null)
+            {
+                var domainManagerField = domain.GetType().GetField("_domainManager", BindingFlags.Instance | BindingFlags.NonPublic);
+                domainManagerField.SetValue(domain, manager);
+            }
+        }
+
+    }
+}
